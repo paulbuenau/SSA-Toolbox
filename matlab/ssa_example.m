805da6acd2939fb3de3a8e213b89768edfc098eb
Jan <jan@jan-laptop.(none)>
2010-10-07 15:48:38 +0200

    Improved ssa_example.m

--- matlab/ssa_example.m
+++ matlab/ssa_example.m
@@ -1,5 +1,9 @@
 function ssa_example
 
+% fixed random seed
+rand('seed', 23);
+randn('seed', 34);
+
 %%%%%%%%%%%%%%%%%%%%
 % generate data set
 %%%%%%%%%%%%%%%%%%%%
@@ -9,29 +13,23 @@ n_changepoints = 5; % number of changepoints
 % random change points
 changepoints = sort(floor(n * rand(n_changepoints, 1)));
 
-% variances
-sigmas = [2; 0.2; 2.5; 0.1; 2.3; 0.1];
-
-% generate non-stationary signal
-Xns = zeros(1, n);
+% generate stationary and non-stationary signal
+X = zeros(2, n);
 for i=1:(n_changepoints + 1)
     if i == 1
-        Xns(1:changepoints(1)) = sigmas(1) * randn(1, changepoints(1));
+        X(:, 1:changepoints(1)) = mvnrnd([0, 3*randn], randcov, changepoints(1))';
     elseif i == (n_changepoints + 1)
-        Xns((changepoints(end) + 1):end) = sigmas(i) * randn(1, n - changepoints(end));
+        X(:, (changepoints(end) + 1):end) = mvnrnd([0, 3*randn], randcov, n - changepoints(end))';
     else
-        Xns((changepoints(i-1) + 1):changepoints(i)) = sigmas(i) * randn(1, changepoints(i) - changepoints(i-1));
+        X(:, (changepoints(i-1) + 1):changepoints(i)) = mvnrnd([0, 3*randn], randcov, changepoints(i) - changepoints(i-1))';
     end
 end
 
-% generate stationary signal
-Xs = randn(1, n);
-
-X = [Xs; Xns];
-
 % random mixing matrix
-sc = 3;
-A = randn(2, 2) .* [1, sc; 1, sc];
+% random angles in [(1/8)*pi, (3/8)*pi]
+alpha = (pi/4)*rand + (1/8)*pi;
+beta = (pi/4)*rand + (1/8)*pi;
+A = [cos(alpha) sin(alpha); cos(beta) sin(beta)] .* sign(randn(2,2));
 
 % mix signals
 Xmixed = A * X;
@@ -45,8 +43,8 @@ Xmixed = A * X;
 % project to stationary and non-stationary subspace
 Xest_s = est_Ps * Xmixed; 
 Xest_ns = est_Pn * Xmixed;
-%Xest_s = Xs;
-%Xest_ns = Xns;
+%Xest_s = X(1, :);
+%Xest_ns = X(2, :);
 
 %%%%%%%%%%%%%%%%%%%%
 % plot signals
@@ -67,18 +65,37 @@ title('Input signal 2');
 line_markers(markers);
 
 % scatter plots of the epochs
+vw = [-10 10];
 eps = n / 4; % epoch size
+cbas = 'black'; % color for basis of s- and n-space
+cebas = 'red'; % color for estimated basis of s- and n-space
+
 subplot(5, 4, 9);
-scatter(Xmixed(1, 1:eps), Xmixed(2, 1:eps));
+scatter(Xmixed(1, 1:eps), Xmixed(2, 1:eps), '.');
+drawaxis(A(:,1), A(:,2), cbas, 10); % basis of s- and n-space
+drawaxis(est_As, est_An, cebas, 10); % basis of estimated s- and n-space
+set(gca, 'Xlim', vw, 'Ylim', vw, 'XTick', [], 'YTick', []);
 title('scatter epoch 1');
+
 subplot(5, 4, 10);
-scatter(Xmixed(1, (eps + 1):(2*eps)), Xmixed(2, (eps + 1):(2*eps)));
+scatter(Xmixed(1, (eps + 1):(2*eps)), Xmixed(2, (eps + 1):(2*eps)), '.');
+drawaxis(A(:,1), A(:,2), cbas, 10); % basis of s- and n-space
+drawaxis(est_As, est_An, cebas, 10); % basis of estimated s- and n-space
+set(gca, 'Xlim', vw, 'Ylim', vw, 'XTick', [], 'YTick', []);
 title('scatter epoch 2');
+
 subplot(5, 4, 11);
-scatter(Xmixed(1, (2*eps + 1):(3*eps)), Xmixed(2, (2*eps + 1):(3*eps)));
+scatter(Xmixed(1, (2*eps + 1):(3*eps)), Xmixed(2, (2*eps + 1):(3*eps)), '.');
+drawaxis(A(:,1), A(:,2), cbas, 10); % basis of s- and n-space
+drawaxis(est_As, est_An, cebas, 10); % basis of estimated s- and n-space
+set(gca, 'Xlim', vw, 'Ylim', vw, 'XTick', [], 'YTick', []);
 title('scatter epoch 3');
+
 subplot(5, 4, 12);
-scatter(Xmixed(1, (3*eps + 1):end), Xmixed(2, (3*eps + 1):end));
+scatter(Xmixed(1, (3*eps + 1):end), Xmixed(2, (3*eps + 1):end), '.');
+drawaxis(A(:,1), A(:,2), cbas, 10); % basis of s- and n-space
+drawaxis(est_As, est_An, cebas, 10); % basis of estimated s- and n-space
+set(gca, 'Xlim', vw, 'Ylim', vw, 'XTick', [], 'YTick', []);
 title('scatter epoch 4');
 
 % estimated stationary and non-stationary signal
@@ -89,7 +106,21 @@ subplot(5, 4, 17:20);
 plot(Xest_ns);
 title('Estimated non-stationary signal');
 
+% random covariance matrix with a fixed stationary part
+function C = randcov
+sig = 1;
+sc = 1;
+m = sqrt(sig*sig/2);
+A = [m m; sc*randn(1, 2)];
+C = A*A';
+
+function drawaxis(x, y, c, s)
+h1 = line([0, s*x(1)], [0, s*x(2)]);
+h2 = line([0, s*y(1)], [0, s*y(2)]);
+set(h1, 'Color', c);
+set(h2, 'Color', c);
+
 function line_markers(x)
 lim = get(gca,'YLim');
-h = arrayfun(@(x) line([x x], lim, 'r'), x);
-% set color?
+h = arrayfun(@(x) line([x x], lim), x);
+set(h, 'Color', 'red');
